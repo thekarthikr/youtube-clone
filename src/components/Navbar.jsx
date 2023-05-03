3c70a0ee52aa614a37fa7a4132b81acab70e6cb1
Karthik <karthikwebdev@outlook.com>
2023-05-03 10:49:47 +0530

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import SearchBar from "../components/SearchBar";
+function Navbar() {
+  return (
+    <Stack
+      direction='row'
+      p={2}
+      justifyContent='space-between
+    '
+      alignItems='center'
+      position='sticky'
+      top='0'
+      sx={{
+        backgroundColor: "hsl(0, 0%, 7%)",
+      }}
+    >
+      <Link>
+        <img width='100px' src={logo} alt='Youtube logo' />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+}
+
+export default Navbar;
