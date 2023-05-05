57eb6a45076d8b5181ba583da91c8c52dc322512
Karthik <karthikwebdev@outlook.com>
2023-05-05 20:20:17 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
+import {
+  Navbar,
+  Feed,
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+} from "./components";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Box sx={{ backgroundColor: "hsl(0, 0%, 7%)" }}>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Feed />} />
+
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </Router>
+  );
 };
 
 export default App;
