6d13d973da3c557bd3e651a5d8ba48de0af36b58
Karthik <karthikwebdev@outlook.com>
2023-05-05 20:16:14 +0530

    add loader

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+function Spinner() {
+  return (
+    <div className='spinner'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Spinner;
